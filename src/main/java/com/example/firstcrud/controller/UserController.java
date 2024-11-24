23bbc2126f71e901165a0e22059545e13487721e
mallquidev <159455849+mallquidev@users.noreply.github.com>
2024-11-24 14:53:33 -0500

    Add user creation controller

--- src/main/java/com/example/firstcrud/controller/UserController.java
+++ src/main/java/com/example/firstcrud/controller/UserController.java
@@ -3,10 +3,12 @@ package com.example.firstcrud.controller;
 import com.example.firstcrud.model.User;
 import com.example.firstcrud.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -16,11 +18,36 @@ public class UserController {
 
     @GetMapping("/")
     public String helloWorld(){
-        return "Hello World jaime chunchaso";
+        return "Hello World";
     }
 
     @GetMapping("/users")
     public List<User> getUsers(){
         return userRepository.findAll();
     }
+
+    //capture by id
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        Optional<User> user = userRepository.findById(id);
+
+        //check if the user exists
+        if(user.isPresent()){
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    //create users with post
+    @PostMapping("/user")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        try {
+            User savedUser = userRepository.save(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
 }
